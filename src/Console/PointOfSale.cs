59ce963054d4b3d491df822b6f681cd092cd2985
Roger Calaf Planell <rogercalaf@gmail.com>
2024-06-09 21:19:37 +0200

    Console App

---
+++ src/Console/PointOfSale.cs
@@ -0,0 +1,57 @@
+﻿using Supermarket.Domain.Aggregates.OrderAggregate;
+using Supermarket.Domain.Aggregates.ProductAggregate;
+using Microsoft.Extensions.Logging;
+
+namespace Supermarket.Console
+{
+    public class PointOfSale
+    {
+        private readonly string _storeId;
+        private readonly string _tillNumber;
+        private readonly ILogger<PointOfSale> _logger;
+        private readonly List<Product> _products;
+        private readonly List<IPricingRule> _pricingRules;
+        private readonly Checkout _checkout;
+
+        public PointOfSale(ILogger<PointOfSale> logger, string storeId, string tillNumber) 
+        {
+            _logger = logger;
+            _products = new List<Product>();
+            _pricingRules = new List<IPricingRule>();
+            _storeId = storeId;
+            _tillNumber = tillNumber;
+            _checkout = new Checkout(_pricingRules);
+
+            _logger.LogInformation("Till number {n} has been activated for store {s}", tillNumber, storeId);
+        }
+
+        public void SetStoreInventory(IEnumerable<Product> products)
+        {
+            _products.AddRange(products);
+
+            _logger.LogInformation("{p} new products has been configured.", products.Count());
+        }
+
+        public void LoadPricingRule(IPricingRule rule)
+        {
+            _pricingRules.Add(rule);
+
+            _logger.LogInformation("New {t} pricing rule with name {n} has been configured", rule.GetType(), rule.Name);
+        }
+
+        public void Scan(Product p)
+        {
+            _checkout.Scan(p);
+
+            _logger.LogInformation("Product {p} has been scanned properly.", p.CodeName);
+        }
+
+        public void ProceedCheckout()
+        {
+            var totalPrice = _checkout.Total();
+
+            _logger.LogInformation("Total Items: {items}", _checkout.Items.Count());
+            _logger.LogInformation("Total Price: {v} {c}", totalPrice.Amount, totalPrice.Currency);
+        }
+    }
+}
---
+++ src/Console/Program.cs
@@ -0,0 +1,58 @@
+﻿using Supermarket.Domain.Aggregates.OrderAggregate;
+using Supermarket.Domain.Aggregates.ProductAggregate;
+using Microsoft.Extensions.Logging;
+using Supermarket.Console;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder
+        .AddFilter("Microsoft", LogLevel.Warning)
+        .AddFilter("System", LogLevel.Warning)
+        .AddFilter("Supermarket", LogLevel.Information)
+        .AddConsole();
+});
+var logger = loggerFactory.CreateLogger<PointOfSale>();
+var pointOfSale = new PointOfSale(logger, "S0001", "T01");
+
+logger.LogInformation("Small Supermarkets - Point of Sale");
+
+//Store Products
+var tea = new Product("GR1", "Green tea", new Price(3.11M, "GBP"));
+var strawberries = new Product("SR1", "Strawberries", new Price(5.0M, "GBP"));
+var coffee = new Product("CF1", "Coffee", new Price(11.23M, "GBP"));
+
+pointOfSale.SetStoreInventory([tea, strawberries, coffee]);
+
+//Define Pricing Rules
+var ceo_rule = new GetOneForFreePricingRule("buy-one-get-one-for-free", tea.Barcode);
+var coo_rule = new BulkPurchasePricingRule(
+    name: "bulk-purchase-x3+-offer", 
+    barcode: strawberries.Barcode, 
+    minQuantity: 3, 
+    multiplier: 0.5M);
+var cto_rule = new BulkPurchasePricingRule(
+    name: "special-offer", 
+    barcode: coffee.Barcode, 
+    minQuantity: 3, 
+    multiplier: Math.Round(coffee.Price.Amount * (1.0M / 3.0M), 2));
+
+pointOfSale.LoadPricingRule(ceo_rule);
+pointOfSale.LoadPricingRule(coo_rule);
+pointOfSale.LoadPricingRule(cto_rule);
+
+logger.LogInformation("Scan any product for checkout.");
+
+pointOfSale.Scan(tea);
+pointOfSale.Scan(strawberries);
+pointOfSale.Scan(strawberries);
+pointOfSale.Scan(strawberries);
+pointOfSale.Scan(strawberries);
+pointOfSale.Scan(coffee);
+pointOfSale.Scan(coffee);
+pointOfSale.Scan(coffee);
+
+pointOfSale.ProceedCheckout();
+
+logger.LogInformation("Thanks for you purchase :)");
+
+
